3cd105ed2e4c7c516c2a2d7166194f94f59b3fd4
Gabriel <gabrielfigueiredobm@hotmail.com>
2025-09-01 13:16:59 -0300

    criação dos usuários, seus tipos e alguns métodos

---
+++ prolog/usuarios.pl
@@ -0,0 +1,37 @@
+:- module(usuarios, [cria_paciente/3, cria_medico/4, get_nome/2, get_cpf/2, get_crm/2, get_senha/2, get_especialidade/2]).
+
+:- use_module(mensagem).
+
+cria_paciente(+Nome, +Cpf, +Senha, -Paciente) :-
+    Paciente = paciente{
+        nome: Nome,
+        cpf: Cpf,
+        senha: Senha,
+        mensagens: [],
+        ultimas_triagens: []
+    }.
+
+cria_medico(+Nome, +Crm, +Senha, +Especialidade, -Medico) :-
+    Medico = medico{
+        nome: Nome,
+        crm: Crm,
+        senha: Senha,
+        especialidade: Especialidade,
+        mensagens: [],
+        triagens: []
+    }.
+
+get_nome(Usuario, Nome) :-
+    Nome = Usuario.nome.
+
+get_senha(Usuario, Senha) :-
+    Senha = Usuario.senha.
+
+get_cpf(Usuario, Cpf) :-
+    Cpf = Usuario.cpf.
+
+get_crm(Usuario, Crm) :-
+    Crm = Usuario.crm.
+
+get_especialidade(Usuario, Especialidade) :-
+    Especialidade = Usuario.especialidade.
